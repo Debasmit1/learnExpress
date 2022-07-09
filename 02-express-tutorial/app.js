c3f98ac31dc656ac7e63c3f736fa58dfef7ec5dc
DebasmitBiswal <debasmitbiswal@outlook.com>
2022-07-10 00:18:38 +0530

    Done for 9/7/22

--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -1,22 +1,22 @@
 const express = require("express");
 const app = express();
+const path = require("path");
 
 //const express = require('express')();
 
-app.get("/", (req, res) => {
-  res.status(200).send("Home Page");
-});
+//setup static and middleware
+app.use(express.static("./public"));
 
-app.get("/about", (req, res) => {
-  res.status(200).send("About Page");
+app.get("/", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "./navbar-app/index.html"));
 });
 
 app.all("*", (req, res) => {
-  res.status(404).send(`<h1>Resource Not found</h1>`);
+  res.status(404).send("Not available");
 });
 
 app.listen(5000, () => {
-  console.log(`Server is live on port 5000`);
+  console.log(`Server is live on 5000`);
 });
 
 //app.get

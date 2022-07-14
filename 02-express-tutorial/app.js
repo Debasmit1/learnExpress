114227908dce450a21d165729e941974711c4127
DebasmitBiswal <debasmitbiswal@outlook.com>
2022-07-15 00:48:44 +0530

    Added authorize

--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -1,23 +1,10 @@
 const express = require("express");
 const app = express();
 const logger = require("./logger");
+const authorize = require("./authorize");
 //req => middleware => res
-// logger is middleware
-// Here in this file we can use logger for different call
 
-// const logger = (req, res, next) => {
-//   const method = req.method;
-//   const url = req.url;
-//   const time = new Date().getFullYear();
-//   console.log(method, url, time);
-//   //res.send("Testing");
-//   next();
-// };
-
-//To use the same function in every call
-//We can use app.use(logger);
-
-app.use(logger);
+app.use([logger, authorize]);
 
 app.get("/", (req, res) => {
   res.send("Home");
